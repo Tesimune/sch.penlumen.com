b1532ab3b2afe79d2763ac0f1982dd3a05f8527d
tesimune <tesimune@gmail.com>
2025-12-05 12:00:42 +0100

    Update

---
+++ src/app/(app)/staff/classes/[uuid]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ClassUpdate from "@/components/app/class-update";
+
+export default function page() {
+    return (
+        <div>
+            <ClassUpdate/>
+        </div>
+    );
+}
