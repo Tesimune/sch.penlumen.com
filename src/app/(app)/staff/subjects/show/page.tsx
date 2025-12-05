952b0a92cc78680fbf13643aa142b31f1600022b
tesimune <tesimune@gmail.com>
2025-12-05 11:46:47 +0100

    Add missing file

---
+++ src/app/(app)/staff/subjects/show/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SubjectUpdate from "@/components/app/subject-update";
+
+export default function page() {
+    return (
+        <div>
+            <SubjectUpdate/>
+        </div>
+    );
+}
